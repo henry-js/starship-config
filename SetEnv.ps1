4152621c31c2f193efc4979ce781f8498aa095f0
James Henry <jhenry@vigosoftware.com>
2024-01-02 13:00:06 +0000

    init commit

---
+++ SetEnv.ps1
@@ -0,0 +1,3 @@
+[Environment]::SetEnvironmentVariable('STARSHIP_CONFIG', '%USERPROFILE%\.config\starship\starship.toml', 'User')
+[Environment]::SetEnvironmentVariable('STARSHIP_CACHE', '%USERPROFILE%\.config\starship\cache', 'User')
+
